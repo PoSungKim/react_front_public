a93efd0bbc277843d21b4554c3e27d09e8d50199
PoSungKim <psungkr94@likelion.org>
2021-11-02 20:46:25 +0900

    Redux-Saga 구축

--- src/modules/reducers/RootReducer.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { CounterReducer } from "./CounterReducer";
-
-const RootReducer = combineReducers({ CounterReducer });
-
-export type RootReducerType = ReturnType<typeof RootReducer>;
-
-export default RootReducer;
