bd44f996a14bcf9e715e031f03b8e9343fa96e57
PoSungKim <psungkr94@likelion.org>
2021-11-04 23:48:24 +0900

    ChatBot CSS

--- src/containers/chatbot/ChatBotContainer.tsx
+++ src/containers/chatbot/ChatBotContainer.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import "../../assets/scss/chatbot.scss";
+import chatBotImg from "../../assets/images/ChatBot.png";
 
-const ChatBotContainer = () => (
-  <>
-    <div id="ChatBotContainer">
-      <div id="ChatBotHeader"></div>
-      <div id="ChatBotContent"></div>
-      <div id="ChatBotTextArea">
-        <input type="text" name="TextArea" id="TextArea" />
+const ChatBotContainer = () => {
+  const style = {
+    backgroundImage: `url(${chatBotImg})`,
+    backgroundPosition: "center",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+  };
+  return (
+    <>
+      <div id="ChatBotContainer">
+        <div id="ChatBotHeader"></div>
+        <div id="ChatBotContent" style={style}></div>
+        <div id="ChatBotTextArea">
+          <input
+            type="text"
+            name="TextArea"
+            id="TextArea"
+            placeholder="Please Type Your Message"
+          />
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default ChatBotContainer;
